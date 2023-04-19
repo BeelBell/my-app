5c69b98aef04b7fcaddab678fb058dfd20315a93
BeelBell <app592374@gmail.com>
2023-04-19 16:02:59 +0700

    1

---
+++ components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import {ArrowForwardIcon} from '@chakra-ui/icons'
+
+
+const Navbar = () => {
+    return (
+        <div className='dark:bg-slate-800 dark:text-white border-b-4 border-stone-200'>
+            <div className=' max-w-[1000px] mx-auto flex justify-between items-center p-4 '>
+                <h1 className='font-Prompt font-medium  text-2xl'>
+                    YaJok Group
+                </h1>
+                <ul className=' font-Prompt font-medium flex '>
+                    <li className='px-5'>
+                        <Link href="/" >
+                            <li>Home</li>
+                        </Link>
+                    </li>
+                    <li className='px-5'>
+                        <Link href="/" >
+                            <li>Our Team</li>
+                        </Link>
+                    </li>
+                    <li className='px-5'>
+                        <Link href="/" >
+                            <li>Our Games</li>
+                        </Link>
+                    </li>
+                </ul>
+                <div>
+                <Button  rightIcon={<ArrowForwardIcon />}  colorScheme='teal' variant='outline'>
+                    Contact Now
+                </Button>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Navbar;
